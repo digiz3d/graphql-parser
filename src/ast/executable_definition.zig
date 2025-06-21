cda7e59ef81a66139e2ce847da93dcf41da124e2
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2025-06-21 22:26:05 +0200

    refactor: simplify executable definition (#33)

--- src/ast/executable_definition.zig
+++ src/ast/executable_definition.zig
@@ -21,29 +21,13 @@ pub const ExecutableDefinition = union(enum) {
 
     pub fn printAST(self: ExecutableDefinition, indent: usize) void {
         switch (self) {
-            ExecutableDefinition.fragmentDefinition => self.fragmentDefinition.printAST(indent),
-            ExecutableDefinition.operationDefinition => self.operationDefinition.printAST(indent),
-            ExecutableDefinition.schemaDefinition => self.schemaDefinition.printAST(indent),
-            ExecutableDefinition.objectTypeDefinition => self.objectTypeDefinition.printAST(indent),
-            ExecutableDefinition.unionTypeDefinition => self.unionTypeDefinition.printAST(indent),
-            ExecutableDefinition.scalarTypeDefinition => self.scalarTypeDefinition.printAST(indent),
-            ExecutableDefinition.directiveDefinition => self.directiveDefinition.printAST(indent),
-            ExecutableDefinition.interfaceTypeDefinition => self.interfaceTypeDefinition.printAST(indent),
-            ExecutableDefinition.schemaExtension => self.schemaExtension.printAST(indent),
+            inline else => |value| value.printAST(indent),
         }
     }
 
     pub fn deinit(self: ExecutableDefinition) void {
         switch (self) {
-            ExecutableDefinition.fragmentDefinition => self.fragmentDefinition.deinit(),
-            ExecutableDefinition.operationDefinition => self.operationDefinition.deinit(),
-            ExecutableDefinition.schemaDefinition => self.schemaDefinition.deinit(),
-            ExecutableDefinition.objectTypeDefinition => self.objectTypeDefinition.deinit(),
-            ExecutableDefinition.unionTypeDefinition => self.unionTypeDefinition.deinit(),
-            ExecutableDefinition.scalarTypeDefinition => self.scalarTypeDefinition.deinit(),
-            ExecutableDefinition.directiveDefinition => self.directiveDefinition.deinit(),
-            ExecutableDefinition.interfaceTypeDefinition => self.interfaceTypeDefinition.deinit(),
-            ExecutableDefinition.schemaExtension => self.schemaExtension.deinit(),
+            inline else => |value| value.deinit(),
         }
     }
 };
