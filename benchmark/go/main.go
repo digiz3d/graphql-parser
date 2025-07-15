de65b92e15f7c597452e54fbbb7e4ad1302ac908
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2025-07-15 20:51:37 +0200

    feat: setup merge benchmark

---
+++ benchmark/go/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	gql "github.com/mununki/gqlmerge/lib"
+)
+
+func main() {
+	typeDefsDir := "../graphql-definitions"
+	files, err := os.ReadDir(typeDefsDir)
+	if err != nil {
+		panic(err)
+	}
+	var typeDefPaths []string
+	for _, file := range files {
+		if !file.IsDir() {
+			typeDefPaths = append(typeDefPaths, typeDefsDir+"/"+file.Name())
+		}
+	}
+	schema := gql.Merge(" ", typeDefPaths...)
+	outputFile, err := os.Create("../go.generated.graphql")
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+	_, err = outputFile.WriteString(*schema)
+	if err != nil {
+		panic(err)
+	}
+}
