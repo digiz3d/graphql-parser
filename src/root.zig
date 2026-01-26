2b0d44fda59b7ce281f4312a28ba97fa3bc325ce
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2026-01-26 21:31:25 +0100

    chore: move benchmark code

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
-const Merger = @import("merge.zig").Merger;
-const Parser = @import("parser.zig").Parser;
-
-pub const merger = Merger;
-pub const parser = Parser;
+pub const Merger = @import("merge.zig").Merger;
+pub const Parser = @import("parser.zig").Parser;
+pub const Printer = @import("printer.zig").Printer;
+pub const Document = @import("ast/document.zig").Document;
+pub const getFileContent = @import("utils/utils.zig").getFileContent;
 
 test {
     std.testing.refAllDecls(@This());
