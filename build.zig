95aa7c6c4ff0e77be0490c17f92c0fa09c3b9ea0
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2026-02-01 21:24:45 +0100

    test build

--- build.zig
+++ build.zig
@@ -1,70 +1,79 @@
 const std = @import("std");
 
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
+
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    for (targets) |target| {
+        const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+        const lib_mod = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = b.resolveTargetQuery(target),
+            .optimize = optimize,
+        });
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+        const exe_mod = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(target),
+            .optimize = optimize,
+        });
 
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "gqlt",
-        .root_module = lib_mod,
-    });
+        const lib = b.addLibrary(.{
+            .linkage = .static,
+            .name = "gqlt",
+            .root_module = lib_mod,
+        });
 
-    b.installArtifact(lib);
+        b.installArtifact(lib);
 
-    const exe = b.addExecutable(.{
-        .name = "gqlt",
-        .root_module = exe_mod,
-    });
+        const exe = b.addExecutable(.{
+            .name = "gqlt",
+            .root_module = exe_mod,
+        });
 
-    b.installArtifact(exe);
+        b.installArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
+        const run_cmd = b.addRunArtifact(exe);
 
-    run_cmd.step.dependOn(b.getInstallStep());
+        run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+
+        const lib_unit_tests = b.addTest(.{
+            .root_module = lib_mod,
+        });
+
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+        const exe_unit_tests = b.addTest(.{
+            .root_module = exe_mod,
+        });
+
+        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_lib_unit_tests.step);
+        test_step.dependOn(&run_exe_unit_tests.step);
+
+        setupBenchmark(b, "benchmark", "benchmark/zig/main.zig", target, optimize, lib_mod);
+        setupBenchmark(b, "micro-benchmark", "benchmark/zig/micro-main.zig", target, optimize, lib_mod);
     }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
-
-    setupBenchmark(b, "benchmark", "benchmark/zig/main.zig", target, optimize, lib_mod);
-    setupBenchmark(b, "micro-benchmark", "benchmark/zig/micro-main.zig", target, optimize, lib_mod);
 }
 
-fn setupBenchmark(b: *std.Build, cliShortcut: []const u8, entrypoint: []const u8, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, lib_mod: *std.Build.Module) void {
+fn setupBenchmark(b: *std.Build, cliShortcut: []const u8, entrypoint: []const u8, target: std.Target.Query, optimize: std.builtin.OptimizeMode, lib_mod: *std.Build.Module) void {
     const benchmark_module = b.createModule(.{
         .root_source_file = b.path(entrypoint),
-        .target = target,
+        .target = b.resolveTargetQuery(target),
         .optimize = optimize,
         .strip = true,
     });
